e6faf44a33c15b3c0d882a25f5e0d49ba72cb173
maxilex <61802609+maxilex@users.noreply.github.com>
2020-04-29 01:01:49 +0400

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,14 +4,14 @@ const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "15e3b196ed5d94f4a41d0304f6b8a680",
+"assets/LICENSE": "bc48f9e67a652faa04e7694b83661ebe",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "39c731e2bbc8b14d8345fcff2d832908",
-"/": "39c731e2bbc8b14d8345fcff2d832908",
-"main.dart.js": "788fa32fc85600949f369b73a47f7be9",
+"index.html": "aac3d8959a6cd60415f2f3bd6b83aea8",
+"/": "aac3d8959a6cd60415f2f3bd6b83aea8",
+"main.dart.js": "151659d5b1e6732f6847f582b0e99082",
 "manifest.json": "27121d87dba7345884a5a6516ba72905"
 };
 
